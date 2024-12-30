c5696fd03a097ad2d5d667ed8f0d73911d328c06
Nikolai <n91056379@gmail.com>
2024-12-30 10:01:40 +0300

    Initial commit

---
+++ src/main/java/by/itiacademy/psekhanikolai/Main.java
@@ -0,0 +1,15 @@
+package by.itiacademy.psekhanikolai;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> city = new ArrayList<>();
+        city.add("Wien");
+        city.add("Rom");
+        city.add("Madrid");
+        city.add("Munchen");
+        city.add("Salzburg");
+        System.out.println(city);
+    }
+}
